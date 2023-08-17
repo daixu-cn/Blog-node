95bef32a3b3cafff55a97287f15fddb7c7630af9
daixu <daixu.cn@outlook.com>
2023-08-17 16:16:15 +0800

    feat: init

---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+/**
+ * @Description: 响应信息
+ * @Author: daixu
+ * @Date: 2023-04-22 21:06:19
+ */
+
+interface IBody {
+  code: number;
+  data?: any;
+  message: string;
+}
---
+++ src/types/processEnv.d.ts
@@ -0,0 +1,37 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    APP_PORT: string;
+    WS_SERVER_PORT: string;
+    SCHEME: string;
+    PORT: string;
+    DOMAIN: string;
+
+    MYSQL_HOST: string;
+    MYSQL_DATABASE: string;
+    MYSQL_USER: string;
+    MYSQL_PASSWORD: string;
+
+    REDIS_PORT: string;
+    REDIS_HOST: string;
+    REDIS_PASSWORD: string;
+    REDIS_FAMILY: string;
+    REDIS_DB: string;
+
+    JWT_SECRET_KEY: string;
+
+    SECRET_QQ_APPID: string;
+    SECRET_QQ_APPKEY: string;
+    SECRET_GITHUB_CLIENTID: string;
+    SECRET_GITHUB_CLIENTSECRET: string;
+
+    EMAIL_HOST: string;
+    EMAIL_PORT: string;
+    EMAIL_USER: string;
+    EMAIL_PASSWORD: string;
+
+    ALIOSS_REGION: string;
+    ALIOSS_ACCESSKEYID: string;
+    ALIOSS_ACCESSKEYSECRET: string;
+    ALIOSS_BUCKET: string;
+  }
+}
