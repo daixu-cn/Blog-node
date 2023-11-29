8b4d9c626446f7848f7e2d86b6b71ec911bea35d
daixu <daixu.cn@outlook.com>
2023-11-29 15:06:09 +0800

    feat(middlewares): 设置响应头信息

--- src/middlewares/koaStatic.ts
+++ src/middlewares/koaStatic.ts
@@ -1,5 +1,7 @@
 import koaStatic from "koa-static";
 import { ASSET_DIR, URL } from "@/config/env";
+import mime from "mime-types";
+import path from "path";
 
 /**
  * @description 静态资源中间件
@@ -10,7 +12,12 @@ export default function (root = `${ASSET_DIR}/`) {
   return koaStatic(root, {
     // 浏览器缓存：30天
     maxAge: 1000 * 60 * 60 * 24 * 30,
-    setHeaders: res => {
+    setHeaders: (res, filePath) => {
+      const contentType = mime.contentType(path.extname(filePath));
+
+      contentType && res.setHeader("Content-Type", contentType);
+      res.setHeader("Content-Disposition", "inline");
+      res.setHeader("Accept-Ranges", "bytes");
       res.setHeader("Author", URL);
     }
   });
