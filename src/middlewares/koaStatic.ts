ae8b9e59d3fe883a8b8ae0a021050f90855455c9
daixu <daixu.cn@outlook.com>
2024-01-09 13:23:24 +0800

    feat: 增加不缓存文件的规则

--- src/middlewares/koaStatic.ts
+++ src/middlewares/koaStatic.ts
@@ -1,7 +1,10 @@
 import koaStatic from "koa-static";
-import { ASSET_DIR, URL } from "@/config/env";
+import { ASSET_DIR, SITE_URL } from "@/config/env";
 import mime from "mime-types";
 import path from "path";
+
+// 不缓存的文件列表
+const noCacheList = ["sitemap.xml", "robots.txt"];
 
 /**
  * @description 静态资源中间件
@@ -13,12 +16,19 @@ export default function (root = `${ASSET_DIR}/`) {
     // 浏览器缓存：30天
     maxAge: 1000 * 60 * 60 * 24 * 30,
     setHeaders: (res, filePath) => {
+      const fileName = filePath.split("/").slice(-1)[0];
+
       const contentType = mime.contentType(path.extname(filePath));
 
       contentType && res.setHeader("Content-Type", contentType);
+      res.setHeader("Author", SITE_URL);
       res.setHeader("Content-Disposition", "inline");
       res.setHeader("Accept-Ranges", "bytes");
-      res.setHeader("Author", URL);
+
+      // 如果数字开头或者在不缓存列表中的文件，则不缓存
+      if (noCacheList.includes(fileName) || !/^\d+\..+$/.test(fileName)) {
+        res.setHeader("Cache-Control", "no-store");
+      }
     }
   });
 }
