8525b4f8ef9bf31aaa36828c17ee34b309b21ffa
daixu <daixu.cn@outlook.com>
2024-01-18 09:02:09 +0800

    feat: 自定义 WebSocket 服务器

--- src/server/ws/index.ts
+++ src/server/ws/index.ts
@@ -1,12 +1,6 @@
 import WebSocketServer from "@/server/ws/WebSocketServer";
 import { server } from "@/server";
 
-const ws = new WebSocketServer(false, { noServer: true });
-
-server.on("upgrade", (request, socket, head) => {
-  ws.getServer().handleUpgrade(request, socket, head, ws => {
-    ws.emit("connection", ws, request);
-  });
-});
+const ws = new WebSocketServer(false, { server });
 
 export default ws;
